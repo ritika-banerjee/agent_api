b0914fb23f9d51b5382c0ed9fe675f123bb4c733
Ritika Banerjee <109010965+ritika-banerjee@users.noreply.github.com>
2025-05-18 13:18:02 +0530

    applied lazy loading

--- app/core/faiss_index.py
+++ app/core/faiss_index.py
@@ -3,17 +3,26 @@ import faiss
 import numpy as np
 from core.kb_loader import load_kb
 
-kb = load_kb()
-faqs = kb["drinks_menu_faq"]
-questions = [q["question"] for q in faqs]
-answers = [q["answer"] for q in faqs]
+model = None
+index = None
+answers = None
 
-model = SentenceTransformer("sentence-transformers/gtr-t5-base")
-embeddings = model.encode(questions)
-index = faiss.IndexFlatL2(embeddings.shape[1])
-index.add(np.array(embeddings))
+def lazy_init():
+    global model, index, answers
+
+    if model is None or index is None or answers is None:
+        kb = load_kb()
+        faqs = kb["drinks_menu_faq"]
+        questions = [q["question"] for q in faqs]
+        answers = [q["answer"] for q in faqs]
+
+        model = SentenceTransformer("sentence-transformers/gtr-t5-base")
+        embeddings = model.encode(questions)
+        index = faiss.IndexFlatL2(embeddings.shape[1])
+        index.add(np.array(embeddings))
 
 def search_faq(query: str):
+    lazy_init()
     vec = model.encode([query])
     _, I = index.search(np.array(vec), 1)
     return {
